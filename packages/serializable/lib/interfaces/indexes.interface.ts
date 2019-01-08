01c0423deb8a1b699fba3636ce22483052fcd50e
Filip Matys <matysfilip@gmail.com>
2019-01-08 14:35:22 +0100

    Build fix for Serializable and version update

--- packages/serializable/lib/interfaces/indexes.interface.ts
+++ packages/serializable/lib/interfaces/indexes.interface.ts
@@ -1,8 +1,8 @@
-// Indexes interface
-export interface IIndexes extends Array<IIndex>  {}
-
 // Index interface
-interface IIndex {
+export interface IIndex {
     fields?: any;
     options?: any;
 }
+
+// Indexes interface
+export interface IIndexes extends Array<IIndex>  {}
