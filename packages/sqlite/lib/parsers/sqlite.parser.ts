5768760af2502cc53aee46836b0d36a98b49bd74
Filip Matys <matysfilip@gmail.com>
2020-09-04 13:15:43 +0200

    SQLite array support

--- packages/sqlite/lib/parsers/sqlite.parser.ts
+++ packages/sqlite/lib/parsers/sqlite.parser.ts
@@ -35,7 +35,20 @@ export class SQLiteParser {
      * @param value 
      */
     private safeObject(value: any): any {
-        return value ? JSON.parse(value) : null;
+        // Check if value is set
+        if (!value) {
+            // Nothing to parse
+            return null;
+        }
+
+        // Check for string
+        if (typeof value !== "string") {
+            // Already parsed
+            return value;
+        }
+
+        // Parse value
+        return JSON.parse(value);
     }
 
     /**
@@ -67,7 +80,7 @@ export class SQLiteParser {
 
         // Return result
         return result;
-    } 
+    }
 
     /**
      * Type to sqlite
@@ -98,35 +111,62 @@ export class SQLiteParser {
      * @param value 
      */
     public fromSQLite(definition: IPropertyDefinition, value: any): any {
-        // Check type
-        switch (definition.type) {
-            // Boolean
-            case PropertyType.BOOLEAN:
-                return !!value;
-            // Date
-            case PropertyType.DATE:
-                return this.safeDate(value);
-            // Double
-            case PropertyType.DOUBLE:
-                return this.safeDouble(value);
-            // Integer
-            case PropertyType.INT:
-                return this.safeInteger(value);
-            // Mixed and embeded
-            case PropertyType.MIXED:
-            case PropertyType.EMBEDDED:
-                return this.safeObject(value);
-            case PropertyType.REFERENCE:
-                return this.safeReference(value);
-            // Buffer, text, oid and string
-            case PropertyType.BUFFER:
-            case PropertyType.TEXT:
-            case PropertyType.OID:
-            case PropertyType.STRING:
-            default:
-                return value;
-                
+        // Parse
+        const parse = (definition: IPropertyDefinition, value: any): any => {
+            // Check type
+            switch (definition.type) {
+                // Boolean
+                case PropertyType.BOOLEAN:
+                    return !!value;
+                // Date
+                case PropertyType.DATE:
+                    return this.safeDate(value);
+                // Double
+                case PropertyType.DOUBLE:
+                    return this.safeDouble(value);
+                // Integer
+                case PropertyType.INT:
+                    return this.safeInteger(value);
+                // Mixed and embeded
+                case PropertyType.MIXED:
+                case PropertyType.EMBEDDED:
+                    return this.safeObject(value);
+                case PropertyType.REFERENCE:
+                    return this.safeReference(value);
+                // Buffer, text, oid and string
+                case PropertyType.BUFFER:
+                case PropertyType.TEXT:
+                case PropertyType.OID:
+                case PropertyType.STRING:
+                default:
+                    return value;
+
+            }
         }
+
+        // Check definition for array
+        if (!definition.isArray) {
+            // Parse value
+            return parse(definition, value);
+        }
+
+        // Init result
+        const result: any[] = [];
+
+        // Get safe value
+        const sValue = this.safeObject(value);
+
+        // Check if sValue is set
+        if (!sValue) {
+            // Return value (not set)
+            return sValue;
+        }
+
+        // Process array
+        (sValue as Array<any>).forEach((item) => result.push(parse(definition, item)));
+
+        // Return result
+        return result;
     }
 
     /**
@@ -135,70 +175,136 @@ export class SQLiteParser {
      * @param value 
      */
     public toSQLite(definition: IPropertyDefinition, value: any): any {
-        // Check type
-        switch (definition.type) {
-            // Boolean
-            case PropertyType.BOOLEAN:
-                return value ? 1 : 0;
-            // Date
-            case PropertyType.DATE:
-                // Check if value is set
-                if (!value) {
-                    return 'null';
-                }
-
-                // Check instance
-                if (value instanceof Date) {
-                    return value.getTime();
-                }
-                else {
-                    return new Date(value).getTime();
-                }
-
-            // Double
-            case PropertyType.DOUBLE:
-                return this.safeDouble(value);
-            // Integer
-            case PropertyType.INT:
-                return this.safeInteger(value);
-            // Mixed and embedded
-            case PropertyType.MIXED:
-            case PropertyType.EMBEDDED:
-                // Check if value is set
-                if (!value) {
-                    return 'null';
-                }
-
-                // Stringify
-                return `'${JSON.stringify(value)}'`;
-            // Reference
-            case PropertyType.REFERENCE:
-                // Check if value is set
-                if (!value) {
-                    return 'null';
-                }
-
-                // Check for string
-                if (value instanceof String) {
-                    return `'${value}'`;
-                }
-                else {
-                    // Check for _id
-                    if (value._id) {
-                        return `'${value._id}'`;
-                    }
-                    else {
+        // Parse function
+        const parse = (definition: IPropertyDefinition, value: any): any => {
+            // Check type
+            switch (definition.type) {
+                // Boolean
+                case PropertyType.BOOLEAN:
+                    return value ? 1 : 0;
+                // Date
+                case PropertyType.DATE:
+                    // Check if value is set
+                    if (!value) {
                         return 'null';
                     }
-                }
 
-            // Buffer, text, oid and string
-            case PropertyType.BUFFER:
-            case PropertyType.TEXT:
-            case PropertyType.OID:
-            case PropertyType.STRING:
-            default:
-                return value ? `'${value}'` : 'null';
+                    // Check instance
+                    if (value instanceof Date) {
+                        return value.getTime();
+                    }
+                    else {
+                        return new Date(value).getTime();
+                    }
+
+                // Double
+                case PropertyType.DOUBLE:
+                    return this.safeDouble(value);
+                // Integer
+                case PropertyType.INT:
+                    return this.safeInteger(value);
+                // Mixed and embedded
+                case PropertyType.MIXED:
+                case PropertyType.EMBEDDED:
+                    // Check if value is set
+                    if (!value) {
+                        return 'null';
+                    }
+
+                    // Stringify
+                    return `'${JSON.stringify(value)}'`;
+                // Reference
+                case PropertyType.REFERENCE:
+                    // Check if value is set
+                    if (!value) {
+                        return 'null';
+                    }
+
+                    // Check for string
+                    if (value instanceof String) {
+                        return `'${value}'`;
+                    }
+                    else {
+                        // Check for _id
+                        if (value._id) {
+                            return `'${value._id}'`;
+                        }
+                        else {
+                            return 'null';
+                        }
+                    }
+
+                // Buffer, text, oid and string
+                case PropertyType.BUFFER:
+                case PropertyType.TEXT:
+                case PropertyType.OID:
+                case PropertyType.STRING:
+                default:
+                    return value ? `'${value}'` : 'null';
+            }
         }
+
+        // Check definition for array
+        if (!definition.isArray) {
+            // Parse value
+            return parse(definition, value);
+        }
+
+        // Check if value is set
+        if (!value) {
+            // No value
+            return 'null';
+        }
+
+        // Init result
+        const result: any[] = [];
+
+        // Property is array, so we need to parse each value
+        (value as Array<any>).forEach((item) => {
+            // Check type
+            switch (definition.type) {
+                // Boolean
+                case PropertyType.BOOLEAN:
+                    return item ? 1 : 0;
+
+                // Date
+                case PropertyType.DATE:
+                    // Check if item is set
+                    if (!item) {
+                        return null;
+                    }
+
+                    // Check for date instance
+                    if (item instanceof Date) {
+                        return item.getTime();
+                    }
+                    else {
+                        return new Date(item).getTime();
+                    }
+
+                // Reference
+                case PropertyType.REFERENCE:
+                    // Process reference, checking for id as 
+                    // the value itself
+                    if (item instanceof String) {
+                        return item;
+                    }
+                    // Check for _id
+                    else if (item._id) {
+                        return item._id;
+                    }
+                    // Invalid value
+                    else {
+                        return null;
+                    }
+
+                // Default value
+                default:
+                    return item ? item : null;
+            }
+        });
+
+        // Stringify result
+        return `'${JSON.stringify(result)}'`;
     }
 }
