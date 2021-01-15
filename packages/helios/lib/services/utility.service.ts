d4de0e2bab99e9f0a517a19c23b5f54fa77a1443
Filip Matys <matysfilip@gmail.com>
2021-01-15 13:05:11 +0100

    Helios utility service

--- packages/helios/lib/services/utility.service.ts
+++ packages/helios/lib/services/utility.service.ts
@@ -1,3 +1,6 @@
+// Interfaces
+import { IHeliosDataSet } from "../interfaces/data-set.interface";
+
 /**
  * Utility service
  * @description Utility methods
@@ -5,4 +8,36 @@
 export class UtilityService {
 
 
+    /**
+     * Map data set to entities
+     * @param dataSet 
+     */
+    public mapDataSetToEntities<TEntity>(dataSet: IHeliosDataSet): TEntity[] {
+        // Init list of entities
+        const entities: TEntity[] = [];
+
+        // Make sure data set is set and has table defined
+        if (!dataSet || !dataSet.table) {
+            // Return empty list of entities
+            return entities;
+        }
+
+        // Process data set rows
+        for (let index = 0; index < (dataSet.table.rows || []).length; index++) {
+            // Get row
+            const row = dataSet.table.rows[index];
+
+            // Init entity
+            const entity: any = {};
+
+            // Map fields
+            row.forEach((field) => entity[field.FieldName] = field.Value);
+
+            // Add entity to list
+            entities.push(entity);
+        }
+
+        // Return entities
+        return entities;
+    }
 }
