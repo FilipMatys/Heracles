73881a2f2d7fc5b46bf2cee41de01617069d75f5
Filip Matys <matysfilip@gmail.com>
2021-03-20 18:14:40 +0100

    SQLite schema parser update

--- packages/sqlite/lib/parsers/schema.parser.ts
+++ packages/sqlite/lib/parsers/schema.parser.ts
@@ -1,7 +1,23 @@
 // External modules
-import { SchemaParser as BaseSchemaParser } from "@calf/serializable";
+import { IPropertyDefinition, ISchema, SchemaParser as BaseSchemaParser } from "@calf/serializable";
 
 /**
  * Schema parser
  */
-export class SchemaParser extends BaseSchemaParser {}
+export class SchemaParser extends BaseSchemaParser {
+
+    /**
+     * Parse target
+     * @param target 
+     */
+     public parse(target: new () => any): ISchema<IPropertyDefinition> {
+        // Get base schema
+        const schema = super.parse(target);
+
+        // Make sure to remove all "." from schema entity name
+        schema.entity.name = schema.entity.name.replace(/\./g, "_");
+
+        // Return schema
+        return schema;
+     }
+}
