aae9bc1ad8abdffbe5a4d3ee0e8673e7ff216e83
Filip Matys <matysfilip@gmail.com>
2021-05-31 09:37:24 +0200

    Update helios procedures

--- packages/helios/lib/modules/procedure/classes/procedures.class.ts
+++ packages/helios/lib/modules/procedure/classes/procedures.class.ts
@@ -5,6 +5,9 @@ import { HeliosProcedure } from "./procedure.class";
 import { HeliosCompaniesProcedureParameters } from "../parameters/companies.parameters";
 import { HeliosCustomOrdersDocumentsProcedureParameters } from "../parameters/custom-orders-documents.parameters";
 import { HeliosCustomOrdersTransactionsProcedureParameters } from "../parameters/custom-orders-transactions.parameters";
+import { HeliosMerchandiseManagementDocumentsProcedureParameters } from "../parameters/merchandise-management-documents.parameters";
+import { HeliosMerchandiseManagementTransactionsProcedureParameters } from "../parameters/merchandise-management-transactions.parameters";
+import { HeliosMerchandiseManagementDocumentsRecalcProcedureParameters } from "../parameters/merchandise-management-documents-recalc.parameters";
 
 /**
  * Helios procedures
@@ -55,6 +58,51 @@ export class HeliosProcedures {
 
     // Custom orders transactions
     private static customOrdersTransactions: HeliosProcedure<HeliosCustomOrdersTransactionsProcedureParameters>;
+
+    /**
+     * Merchandise management documents
+     * @description Merchandise management documents
+     */
+    public static get MerchandiseManagementDocuments(): HeliosProcedure<HeliosMerchandiseManagementDocumentsProcedureParameters> {
+        // Make sure documents is set
+        this.merchandiseManagementDocuments = this.merchandiseManagementDocuments || new HeliosProcedure("MerchandiseManagementDocuments", HeliosMerchandiseManagementDocumentsProcedureParameters);
+
+        // Return merchandise management documents
+        return this.merchandiseManagementDocuments;
+    }
+
+    // Merchandise management documents
+    private static merchandiseManagementDocuments: HeliosProcedure<HeliosMerchandiseManagementDocumentsProcedureParameters>;
+
+    /**
+     * Merchandise management documents recalc
+     * @description Merchandise management documents recalc
+     */
+    public static get MerchandiseManagementDocumentsRecalc(): HeliosProcedure<HeliosMerchandiseManagementDocumentsRecalcProcedureParameters> {
+        // Make sure documents is set
+        this.merchandiseManagementDocumentsRecalc = this.merchandiseManagementDocumentsRecalc || new HeliosProcedure("MerchandiseManagementDocumentsRecalc", HeliosMerchandiseManagementDocumentsRecalcProcedureParameters);
+
+        // Return merchandise management documents recalc
+        return this.merchandiseManagementDocumentsRecalc;
+    }
+
+    // Merchandise management documents recalc
+    private static merchandiseManagementDocumentsRecalc: HeliosProcedure<HeliosMerchandiseManagementDocumentsRecalcProcedureParameters>;
+
+    /**
+     * Merchandise management transactions
+     * @description Merchandise management transactions
+     */
+    public static get MerchandiseManagementTransactions(): HeliosProcedure<HeliosMerchandiseManagementTransactionsProcedureParameters> {
+        // Make sure transactions is set
+        this.merchandiseManagementTransactions = this.merchandiseManagementTransactions || new HeliosProcedure("MerchandiseManagementTransactions", HeliosMerchandiseManagementTransactionsProcedureParameters);
+
+        // Return merchandise management transactions
+        return this.merchandiseManagementTransactions;
+    }
+
+    // Merchandise management transactions
+    private static merchandiseManagementTransactions: HeliosProcedure<HeliosMerchandiseManagementTransactionsProcedureParameters>;
 
     /**
      * Build procedure name
